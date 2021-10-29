6ec30b67e633554475b82d19561c255072335ba8
gruve-p <jackielove4u@hotmail.com>
2021-10-29 15:18:48 +0200

    Fixes after merging upstream

--- BTCPayServer/Models/StoreViewModels/LightningSettingsViewModel.cs
+++ BTCPayServer/Models/StoreViewModels/LightningSettingsViewModel.cs
@@ -5,7 +5,7 @@ namespace BTCPayServer.Models.StoreViewModels
     public class LightningSettingsViewModel : LightningNodeViewModel
     {
         // Payment
-        [Display(Name = "Display Lightning payment amounts in Satoshis")]
+        [Display(Name = "Display Lightning payment amounts in Gros")]
         public bool LightningAmountInSatoshi { get; set; }
 
         [Display(Name = "Add hop hints for private channels to the Lightning invoice")]
@@ -16,20 +16,20 @@ namespace BTCPayServer.Models.StoreViewModels
 
         [Display(Name = "Description template of the lightning invoice")]
         public string LightningDescriptionTemplate { get; set; }
-        
+
         // LNURL
         [Display(Name = "Enable LNURL")]
         public bool LNURLEnabled { get; set; }
-        
+
         [Display(Name = "LNURL Classic Mode")]
         public bool LNURLBech32Mode { get; set; } = true;
 
         [Display(Name = "LNURL enabled for standard invoices")]
         public bool LNURLStandardInvoiceEnabled { get; set; } = true;
-        
+
         [Display(Name = "Allow payee to pass a comment")]
         public bool LUD12Enabled { get; set; }
-        
+
         [Display(Name = "Do not offer BOLT11 for standard invoices")]
         public bool DisableBolt11PaymentMethod { get; set; }
     }
