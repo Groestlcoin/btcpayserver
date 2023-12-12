be72eb8398a20bd45b995cba2101743b8d92203d
gruve-p <jackielove4u@hotmail.com>
2023-12-12 18:48:12 +0100

    Update UIPullPaymentController.Boltcard.cs

--- BTCPayServer/Controllers/UIPullPaymentController.Boltcard.cs
+++ BTCPayServer/Controllers/UIPullPaymentController.Boltcard.cs
@@ -75,10 +75,10 @@ next:
                         var permission = await vaultClient.AskPermission(VaultServices.NFC, cts.Token);
                         if (permission is null)
                         {
-                            await vaultClient.Show(VaultMessageType.Error, "BTCPay Server Vault does not seem to be running, you can download it on <a target=\"_blank\" href=\"https://github.com/btcpayserver/BTCPayServer.Vault/releases/latest\">Github</a>.", cts.Token);
+                            await vaultClient.Show(VaultMessageType.Error, "GRSPay Server Vault does not seem to be running, you can download it on <a target=\"_blank\" href=\"https://github.com/Groestlcoin/GRSPayServer.Vault/releases/latest\">Github</a>.", cts.Token);
                             goto next;
                         }
-                        await vaultClient.Show(VaultMessageType.Ok, "BTCPayServer successfully connected to the vault.", cts.Token);
+                        await vaultClient.Show(VaultMessageType.Ok, "GRSPayServer successfully connected to the vault.", cts.Token);
                         if (permission is false)
                         {
                             await vaultClient.Show(VaultMessageType.Error, "The user declined access to the vault.", cts.Token);
